8969383e9378e37fce44de1c674c7594b783a817
Adam Miller <adam.miller@csgi.com>
2014-08-25 13:34:19 -0500

    fix refresh issue

--- sortableListBox/directive.js
+++ sortableListBox/directive.js
@@ -42,6 +42,7 @@
                             });
 
                             scope.items = newItemsArray;
+                            scope.$apply();
                         });
                     }
 
